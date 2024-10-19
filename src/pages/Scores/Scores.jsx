0b9594210c673bea83c0384878b9874a552745b9
alziputra <alziputra12@gmail.com>
2024-10-19 12:35:40 +0700

    update score view

--- src/pages/Scores/Scores.jsx
+++ src/pages/Scores/Scores.jsx
@@ -23,91 +23,105 @@ const Scores = () => {
 
   return (
     <div className="container mx-auto p-4">
-      <div className="bg-white border-2 border-black shadow-[4px_4px_0px_rgba(0,0,0,1)] rounded-lg p-6">
-        <h2 className="text-2xl font-bold mb-6">Penilaian Atlet</h2>
+      <div className="bg-white border-2 border-black shadow-[4px_4px_0px_rgba(0,0,0,1)] rounded-lg p-4">
+        <div className="border-2 border-black rounded-lg p-6 mb-3">
+          <h2 className="text-2xl font-bold mb-6">Penilaian Atlet</h2>
 
-        {/* Select Competition */}
-        <div className="mb-4">
-          <label className="block font-bold mb-2">Pilih Pertandingan</label>
-          <select value={selectedCompetition} onChange={handleCompetitionChange} className="select select-bordered w-full">
-            <option value="" disabled>
-              Pilih Pertandingan
-            </option>
-            {matches
-              .filter((match, index, self) => index === self.findIndex((m) => m.competition_id === match.competition_id))
-              .map((match) => (
-                <option key={match.competition_id} value={match.competition_id}>
-                  {match.competition_name}
+          <div className="grid grid-cols-2 gap-4">
+            {/* Select Competition */}
+            <div className="mb-6">
+              <label className="block font-bold mb-2">Pilih Pertandingan</label>
+              <select value={selectedCompetition} onChange={handleCompetitionChange} className="select select-bordered w-full">
+                <option value="" disabled>
+                  Pilih Pertandingan
                 </option>
-              ))}
-          </select>
-        </div>
-
-        {/* Select Athletes */}
-        <div className="grid grid-cols-2 gap-4 mb-6">
-          <div>
-            <label className="block font-bold mb-2">Pilih Atlet</label>
-            <select
-              value={selectedAthlete}
-              onChange={(e) => setSelectedAthlete(e.target.value)}
-              className="select select-bordered w-full"
-              disabled={!selectedCompetition} // Disable if no competition is selected
-            >
-              <option value="" disabled>
-                Pilih Atlet
-              </option>
-              {matches
-                .filter((match) => match.competition_id.toString() === selectedCompetition) // Filter atlet berdasarkan kompetisi yang dipilih
-                .map((match) => (
-                  <option key={match.athlete1_id} value={match.athlete1_id}>
-                    {match.athlete1_name} VS {match.athlete2_name}
+                {matches
+                  .filter((match, index, self) => index === self.findIndex((m) => m.competition_id === match.competition_id))
+                  .map((match) => (
+                    <option key={match.competition_id} value={match.competition_id}>
+                      {match.competition_name}
+                    </option>
+                  ))}
+              </select>
+            </div>
+            {/* Select Athletes */}
+            <div className="mb-6">
+              <div>
+                <label className="block font-bold mb-2">Pilih Atlet</label>
+                <select
+                  value={selectedAthlete}
+                  onChange={(e) => setSelectedAthlete(e.target.value)}
+                  className="select select-bordered w-full"
+                  disabled={!selectedCompetition} // Disable if no competition is selected
+                >
+                  <option value="" disabled>
+                    Pilih Atlet
                   </option>
-                ))}
-            </select>
+                  {matches
+                    .filter((match) => match.competition_id.toString() === selectedCompetition) // Filter atlet berdasarkan kompetisi yang dipilih
+                    .map((match) => (
+                      <option key={match.athlete1_id} value={match.athlete1_id}>
+                        {match.athlete1_name} VS {match.athlete2_name}
+                      </option>
+                    ))}
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <button className="flex btn btn-sm bg-[#2ac34b] hover:bg-[#74f590] text-white hover:text-black border-2 border-black items-center hover:shadow-[2px_2px_0px_rgba(0,0,0,1)]">
+            <a href="">Masuk penilaian</a>
+          </button>
+        </div>
+        
+        {/* Display Selected Choices */}
+        <div className="border-2 border-black rounded-lg p-6">
+          <div className="mb-4">
+            <div className="flex justify-between gap-2">
+              <div className="flex flex-col items-center w-1/2 bg-blue-500 p-4 rounded-lg">
+                <div className="text-white">
+                  {selectedAthlete
+                    ? (() => {
+                        const match = matches.find((match) => match.athlete1_id.toString() === selectedAthlete || match.athlete2_id.toString() === selectedAthlete);
+                        if (match) {
+                          return match.athlete1_id.toString() === selectedAthlete ? match.athlete1_name : match.athlete2_name;
+                        }
+                        return "Belum dipilih";
+                      })()
+                    : "Belum dipilih"}
+                </div>
+                <div className="flex flex-col mt-4 gap-2">
+                  <button className="btn btn-primary">Kick Score</button>
+                  <button className="btn btn-primary">Punch Score</button>
+                  <button className="btn btn-primary">Elbow Score</button>
+                  <button className="btn btn-primary">Knee Score</button>
+                  <button className="btn btn-primary">Throw Score</button>
+                </div>
+              </div>
+
+              <div className="flex flex-col items-center w-1/2 bg-red-500 p-4 rounded-lg">
+                <div className="text-white">
+                  {selectedAthlete
+                    ? (() => {
+                        const match = matches.find((match) => match.athlete1_id.toString() === selectedAthlete || match.athlete2_id.toString() === selectedAthlete);
+                        if (match) {
+                          return match.athlete1_id.toString() === selectedAthlete ? match.athlete2_name : match.athlete1_name;
+                        }
+                        return "Belum dipilih";
+                      })()
+                    : "Belum dipilih"}
+                </div>
+                <div className="flex flex-col mt-4 gap-2">
+                  <button className="btn btn-primary">Kick Score</button>
+                  <button className="btn btn-primary">Punch Score</button>
+                  <button className="btn btn-primary">Elbow Score</button>
+                  <button className="btn btn-primary">Knee Score</button>
+                  <button className="btn btn-primary">Throw Score</button>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-
-        {/* Display Selected Choices */}
-        <div className="mb-4">
-          <h3 className="text-xl font-bold mb-4">Pilihan Anda:</h3>
-          <p>
-            <strong>Pertandingan:</strong> {selectedCompetition ? selectedCompetition : "Belum dipilih"}
-          </p>
-          <p>
-            <strong>Atlet Biru:</strong> {selectedAthlete ? selectedAthlete : "Belum dipilih"}
-          </p>
-        </div>
-
-        {/* Render Skor dan Data Lainnya */}
-        <h3 className="text-xl font-bold mb-4">Skor</h3>
-        {scores.length === 0 ? (
-          <p>Tidak ada skor untuk ditampilkan.</p>
-        ) : (
-          <table className="table-auto w-full">
-            <thead>
-              <tr>
-                <th className="border px-4 py-2">Atlet</th>
-                <th className="border px-4 py-2">Poin Tendangan</th>
-                <th className="border px-4 py-2">Poin Pukulan</th>
-                <th className="border px-4 py-2">Poin Elbow</th>
-                <th className="border px-4 py-2">Poin Lutut</th>
-                <th className="border px-4 py-2">Poin Lemparan</th>
-              </tr>
-            </thead>
-            <tbody>
-              {scores.map((score) => (
-                <tr key={score.score_id}>
-                  <td className="border px-4 py-2">{score.athlete_name}</td>
-                  <td className="border px-4 py-2">{score.kick_score}</td>
-                  <td className="border px-4 py-2">{score.punch_score}</td>
-                  <td className="border px-4 py-2">{score.elbow_score}</td>
-                  <td className="border px-4 py-2">{score.knee_score}</td>
-                  <td className="border px-4 py-2">{score.throw_score}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        )}
       </div>
     </div>
   );
