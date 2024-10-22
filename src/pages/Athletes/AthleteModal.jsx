d57bb3a01511204ff470878a481c68705b42cf65
alziputra <alziputra12@gmail.com>
2024-10-22 23:45:27 +0700

    create paginattion and search

--- src/pages/Athletes/AthleteModal.jsx
+++ src/pages/Athletes/AthleteModal.jsx
@@ -1,7 +1,7 @@
 import { useState, useContext, useEffect } from "react";
 import PropTypes from "prop-types";
 import { AthleteContext } from "../../context/AthleteContext";
-import { FaTimes } from "react-icons/fa";
+import { FaTimes, FaCheck } from "react-icons/fa";
 import toast from "react-hot-toast";
 
 const AthleteModal = ({ isOpen, setIsOpen, athlete }) => {
@@ -48,27 +48,39 @@ const AthleteModal = ({ isOpen, setIsOpen, athlete }) => {
 
   return (
     <div className={`modal ${isOpen ? "modal-open" : ""}`}>
-      <div className="modal-box mx-auto border-2 border-slate-700 shadow-[4px_4px_0px_rgba(0,0,0,1)] p-4 max-h-[calc(100vh-8rem)] overflow-hidden bg-[#F5F5DC]">
-        <div className="flex justify-between items-center mb-4">
+      <div className="modal-box mx-auto border-2 border-slate-700 shadow-[4px_4px_0px_rgba(0,0,0,1)] p-4 max-h-screen overflow-y-auto bg-[#F5F5DC]">
+        <div className="flex justify-between items-center p-2">
           <h2 className="text-lg font-bold text-black">{athlete ? "Edit Athlete" : "Add Athlete"}</h2>
-          <button className="btn bg-red-500 text-black border-2 border-slate-700 hover:bg-red-600 focus:shadow-[2px_2px_0px_rgba(0,0,0,1)]" onClick={() => setIsOpen(false)}>
-            <FaTimes />
-          </button>
+          <div className="flex gap-2">
+            {/* Close Button */}
+            <button type="button" className="btn btn-sm bg-red-500 text-black border-2 border-slate-700 hover:bg-red-600 focus:shadow-[2px_2px_0px_rgba(0,0,0,1)]" onClick={() => setIsOpen(false)}>
+              <FaTimes />
+            </button>
+
+            {/* Submit Button */}
+            <button
+              type="submit"
+              form="athleteForm" // Connect button with form ID
+              className="btn btn-sm bg-blue-500 text-black border-2 border-slate-700 hover:bg-blue-600 focus:shadow-[2px_2px_0px_rgba(0,0,0,1)]"
+            >
+              <FaCheck />
+            </button>
+          </div>
         </div>
 
         {/* Form yang bisa di-scroll */}
-        <form onSubmit={handleSubmit} className="overflow-y-auto max-h-[60vh] pr-2">
+        <form id="athleteForm" onSubmit={handleSubmit} className="overflow-y-auto max-h-[60vh] pr-2">
           <div className="form-control mb-2">
             <label className="label">
               <span className="label-text text-black">Name</span>
             </label>
-            <input type="text" className="input bg-slate-500 focus:shadow-[inset_0_0_5px_rgba(0,0,0,0.8),_inset_0_0_10px_rgba(255,255,255,0.2)] text-white" value={name} onChange={(e) => setName(e.target.value)} required />
+            <input type="text" className="input input-sm bg-slate-500 focus:shadow-[inset_0_0_5px_rgba(0,0,0,0.8),_inset_0_0_10px_rgba(255,255,255,0.2)] text-white" value={name} onChange={(e) => setName(e.target.value)} required />
           </div>
           <div className="form-control mb-2">
             <label className="label">
               <span className="label-text text-black">Team</span>
             </label>
-            <select className="select bg-slate-500 focus:shadow-[inset_0_0_5px_rgba(0,0,0,0.8),_inset_0_0_10px_rgba(255,255,255,0.2)] text-white" value={team} onChange={(e) => setTeam(e.target.value)} required>
+            <select className="select select-sm bg-slate-500 focus:shadow-[inset_0_0_5px_rgba(0,0,0,0.8),_inset_0_0_10px_rgba(255,255,255,0.2)] text-white" value={team} onChange={(e) => setTeam(e.target.value)} required>
               <option value="" disabled>
                 Select a team
               </option>
@@ -80,7 +92,7 @@ const AthleteModal = ({ isOpen, setIsOpen, athlete }) => {
             <label className="label">
               <span className="label-text text-black">Martial Art</span>
             </label>
-            <input type="text" className="input bg-slate-500 focus:shadow-[inset_0_0_5px_rgba(0,0,0,0.8),_inset_0_0_10px_rgba(255,255,255,0.2)] text-white" value={martial} onChange={(e) => setMartial(e.target.value)} required />
+            <input type="text" className="input input-sm bg-slate-500 focus:shadow-[inset_0_0_5px_rgba(0,0,0,0.8),_inset_0_0_10px_rgba(255,255,255,0.2)] text-white" value={martial} onChange={(e) => setMartial(e.target.value)} required />
           </div>
           <div className="form-control mb-2">
             <label className="label">
@@ -88,7 +100,7 @@ const AthleteModal = ({ isOpen, setIsOpen, athlete }) => {
             </label>
             <input
               type="text"
-              className="input bg-slate-500 focus:shadow-[inset_0_0_5px_rgba(0,0,0,0.8),_inset_0_0_10px_rgba(255,255,255,0.2)] text-white"
+              className="input input-sm bg-slate-500 focus:shadow-[inset_0_0_5px_rgba(0,0,0,0.8),_inset_0_0_10px_rgba(255,255,255,0.2)] text-white"
               placeholder="cm"
               value={height}
               onChange={(e) => setHeight(e.target.value)}
@@ -101,19 +113,12 @@ const AthleteModal = ({ isOpen, setIsOpen, athlete }) => {
             </label>
             <input
               type="text"
-              className="input bg-slate-500 focus:shadow-[inset_0_0_5px_rgba(0,0,0,0.8),_inset_0_0_10px_rgba(255,255,255,0.2)] text-white"
+              className="input input-sm bg-slate-500 focus:shadow-[inset_0_0_5px_rgba(0,0,0,0.8),_inset_0_0_10px_rgba(255,255,255,0.2)] text-white"
               placeholder="kg"
               value={weight}
               onChange={(e) => setWeight(e.target.value)}
               required
             />
-          </div>
-
-          {/* Submit button */}
-          <div className="modal-action">
-            <button type="submit" className="btn bg-[#A6FAFF] text-black border-2 border-slate-700 hover:bg-[#62f2f2] hover:text-black">
-              {athlete ? "Save Changes" : "Add Athlete"}
-            </button>
           </div>
         </form>
       </div>
--- src/pages/Athletes/Athletes.jsx
+++ src/pages/Athletes/Athletes.jsx
@@ -1,6 +1,6 @@
 import { useContext, useState } from "react";
 import { AthleteContext } from "../../context/AthleteContext";
-import { FaEdit, FaTrashAlt, FaPlus } from "react-icons/fa";
+import { FaEdit, FaTrashAlt, FaPlus, FaSearch } from "react-icons/fa";
 import AthleteModal from "./AthleteModal";
 import ModalConfirmation from "../../components/ModalConfirmation";
 import toast from "react-hot-toast";
@@ -10,6 +10,25 @@ const Athletes = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isConfirmOpen, setIsConfirmOpen] = useState(false);
   const [selectedAthlete, setSelectedAthlete] = useState(null);
+
+  // Buat state untuk pagination
+  const [currentPage, setCurrentPage] = useState(1);
+  const recordsPerPage = 5; // Atur jumlah data per halaman
+
+  // Search state
+  const [searchQuery, setSearchQuery] = useState("");
+
+  // Buat fungsi untuk handle search query
+  const handleSearch = (e) => {
+    setSearchQuery(e.target.value);
+    setCurrentPage(1); // Reset menjadi halaman pertama setiap kali search query berubah
+  };
+
+  // Filter atlit berdasarkan search query
+  const filteredAthletes = athletes.filter((athlete) => athlete.name.toLowerCase().includes(searchQuery.toLowerCase()));
+  const totalPages = Math.ceil(filteredAthletes.length / recordsPerPage); // Jumlah halaman berdasarkan hasil yang difilter
+  // Buat fungsi untuk mendapatkan record terkini untuk pagination
+  const currentAthletes = filteredAthletes.slice((currentPage - 1) * recordsPerPage, currentPage * recordsPerPage);
 
   const handleAdd = () => {
     setSelectedAthlete(null); // Reset untuk mode add
@@ -37,19 +56,39 @@ const Athletes = () => {
     }
   };
 
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   if (loading) return <p className="text-center">Loading...</p>;
   if (error) return <p className="text-center text-red-500">{error}</p>;
 
   return (
     <div className="container mx-auto p-4">
       <div className="bg-white border-2 border-black shadow-[4px_4px_0px_rgba(0,0,0,1)] rounded-lg p-6">
-        <div className="flex justify-between items-center mb-4">
+        <div className="flex justify-between items-center">
           <h2 className="text-2xl font-bold text-slate-600">Athletes</h2>
-          <button className="flex btn btn-sm bg-[#2ac34b] hover:bg-[#74f590] text-white hover:text-black border-2 border-black items-center hover:shadow-[2px_2px_0px_rgba(0,0,0,1)]" onClick={handleAdd}>
-            <FaPlus/>
-            Add Athlete
+        </div>
+        <div className="flex gap-2 justify-end p-2">
+          <div className="relative w-full max-w-xs">
+            <span className="absolute inset-y-0 left-0 pl-2 flex items-center text-gray-400">
+              <FaSearch />
+            </span>
+            <input
+              type="text"
+              className="input input-sm pl-8 border-2 border-black w-full bg-[#F5F5DC] placeholder-gray-400 focus:outline-none focus:ring-1 focus:ring-black focus:border-black"
+              placeholder="Search by name"
+              value={searchQuery}
+              onChange={handleSearch}
+            />
+          </div>
+
+          <button className="flex btn btn-sm bg-[#2ac34b] hover:bg-[#74f590] text-white hover:text-black border-2 border-black hover:shadow-[2px_2px_0px_rgba(0,0,0,1)]" onClick={handleAdd}>
+            <FaPlus />
+            Add
           </button>
         </div>
+
         <div className="overflow-x-auto">
           <table className="table w-full bg-[#F5F5DC] text-zinc-700 border-4 border-black shadow-[8px_8px_0px_rgba(0,0,0,1)]">
             <thead className="bg-[#F4A460] border-b-4 border-black text-gray-700">
@@ -64,26 +103,45 @@ const Athletes = () => {
               </tr>
             </thead>
             <tbody>
-              {athletes.map((athlete, index) => (
-                <tr key={athlete.athlete_id} className="hover:bg-[#e3dd9a]">
-                  <td className="border-black border-r-2 p-2 text-center">{index + 1}</td>
-                  <td className="border-black border-r-2 p-2">{athlete.name}</td>
-                  <td className="border-black border-r-2 p-2">{athlete.team}</td>
-                  <td className="border-black border-r-2 p-2">{athlete.martial}</td>
-                  <td className="border-black border-r-2 p-2">{athlete.height} cm</td>
-                  <td className="border-black border-r-2 p-2">{athlete.weight} kg</td>
-                  <td className="flex justify-center space-x-2 p-2">
-                    <button className="btn bg-[#378CE7] text-white border-2 border-[#378CE7] hover:border-black hover:bg-[#5aa9ff] btn-xs" onClick={() => handleEdit(athlete)}>
-                      <FaEdit />
-                    </button>
-                    <button className="btn bg-[#FF204E] text-white border-2 border-[#FF204E] hover:border-black hover:bg-[#ff5b7c] hover:text-white btn-xs" onClick={() => handleDelete(athlete)}>
-                      <FaTrashAlt />
-                    </button>
+              {currentAthletes.length > 0 ? (
+                currentAthletes.map((athlete, index) => (
+                  <tr key={athlete.athlete_id} className="hover:bg-[#e3dd9a]">
+                    <td className="border-black border-r-2 p-2 text-center">{(currentPage - 1) * recordsPerPage + index + 1}</td>
+                    <td className="border-black border-r-2 p-2">{athlete.name}</td>
+                    <td className="border-black border-r-2 p-2">{athlete.team}</td>
+                    <td className="border-black border-r-2 p-2">{athlete.martial}</td>
+                    <td className="border-black border-r-2 p-2">{athlete.height} cm</td>
+                    <td className="border-black border-r-2 p-2">{athlete.weight} kg</td>
+                    <td className="flex justify-center space-x-2 p-2">
+                      <button className="btn bg-[#378CE7] text-white border-2 border-[#378CE7] hover:border-black hover:bg-[#5aa9ff] btn-xs" onClick={() => handleEdit(athlete)}>
+                        <FaEdit />
+                      </button>
+                      <button className="btn bg-[#FF204E] text-white border-2 border-[#FF204E] hover:border-black hover:bg-[#ff5b7c] hover:text-white btn-xs" onClick={() => handleDelete(athlete)}>
+                        <FaTrashAlt />
+                      </button>
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td colSpan="7" className="text-center py-4">
+                    No athletes found.
                   </td>
                 </tr>
-              ))}
+              )}
             </tbody>
           </table>
+        </div>
+
+        {/* Pagination */}
+        <div className="flex justify-center mt-4 gap-2">
+          <div className="flex btn-group gap-1  ">
+            {Array.from({ length: totalPages }, (_, i) => (
+              <button key={i + 1} className={`btn btn-sm bg-[#F5F5DC] text-zinc-700 ${currentPage === i + 1 ? "btn-active" : ""}`} onClick={() => handlePageChange(i + 1)}>
+                {i + 1}
+              </button>
+            ))}
+          </div>
         </div>
 
         {isModalOpen && <AthleteModal isOpen={isModalOpen} setIsOpen={setIsModalOpen} athlete={selectedAthlete} />}
